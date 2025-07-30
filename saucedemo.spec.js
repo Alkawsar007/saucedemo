c67d967c27e14cb08e952525711b869bc9672f28
NUR MOHAMMAD AL KAWSAR <nurmohammadalkawsar007@gmail.com>
2025-07-30 22:24:10 +0600

    add comment

--- saucedemo.spec.js
+++ saucedemo.spec.js
@@ -5,13 +5,13 @@ const { chromium } = require('playwright');
     const browser = await chromium.launch({ headless: false });
     const page = await browser.newPage();
 
-
+    //go to the url
     await page.goto('https://www.saucedemo.com/');
 
-
+    // enter username & password
     await page.fill('#user-name', 'standard_user');
     await page.fill('#password', 'secret_sauce');
     await page.click('#login-button');
 
-  
+
 })();
